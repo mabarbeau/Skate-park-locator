063cd597f1665705fe0637a5cc789a63bf39c774
Marc Barbeau <hello@marcbarbeau.com>
2016-09-22 21:49:50 -0600

    Define spot factory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -21,3 +21,23 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Spot::class, function (Faker\Generator $faker) {
+    return [
+      'name' => $faker->unique()->catchPhrase,
+      'title' => $faker->sentence($nbWords = 6, $variableNbWords = true),
+      'description' => $faker->sentence($nbWords = 20, $variableNbWords = true),
+      'address' => $faker->streetAddress,
+      'locality' => $faker->city,
+      'reagion' => $faker->state,
+      'postcode' => $faker->postcode,
+      'country' => $faker->countryCode,
+      'lat' => $faker->latitude,
+      'lng' => $faker->longitude,
+      'votes' => $faker->numberBetween($min = 1, $max = 10000),
+      'hearts' => $faker->numberBetween($min = 1, $max = 1000),
+      'rating' => $faker->numberBetween($min = 1, $max = 5),
+      'creator_id' => $faker->numberBetween($min = 1, $max = 100),
+      'updater_id' => $faker->numberBetween($min = 1, $max = 100),
+    ];
+});
