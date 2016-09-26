2043744601c4165686eb0ec4ca61cca5b109de1c
Marc Barbeau <hello@marcbarbeau.com>
2016-09-26 14:08:00 -0600

    Create rest function ()

--- app/Http/Controllers/Spots.php
+++ app/Http/Controllers/Spots.php
@@ -5,11 +5,42 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Spot;
 
 class Spots extends Controller
 {
-    protected function home()
+    protected function index()
     {
-       return view('pages.spots', ['user' => 'User name']);
+      $spots = Spot::all();
+
+      return view('pages.spots.index', compact('spots'));
+    }
+
+    protected function show($slug)
+    {
+      $spot = Spot::where('slug', $slug)->firstOrFail();
+
+      return view('pages.spots.show', compact('spot'));
+    }
+
+    protected function create()
+    {
+      return view('pages.spots.create');
+    }
+
+    protected function store(Request $request)
+    {
+      $input = $request->all();
+      $input['votes'] = 0;
+      $input['hearts']= 0;
+      $input['rating'] = null;
+      $input['creator_id'] = 1;
+      $input['updater_id'] = 10;
+
+      // var_dump($input);
+
+      Spot::create($input);
+
+      return redirect('spots');
     }
 }
