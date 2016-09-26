17a093cb920332c7617bf57ed0ef1ad07154745e
Marc Barbeau <hello@marcbarbeau.com>
2016-09-25 20:11:45 -0600

    Add jQuery cdn

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -12,6 +12,8 @@
 
         <title>@yield('title')</title>
 
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+
         <!-- Styles -->
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
