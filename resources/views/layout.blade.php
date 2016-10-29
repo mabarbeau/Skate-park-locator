6e0b3b90b04abd24a042f60357b13f693e082deb
Marc Barbeau <hello@marcbarbeau.com>
2016-10-29 11:36:08 -0600

    Delete comment

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,11 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <!-- (Drag and drop code here)
-
-        -->
-
-
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
