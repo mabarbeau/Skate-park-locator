05d306fc226305d9c1745a22ba8dfe9580c32fa8
Marc Barbeau <mabarbeau1@gmail.com>
2017-05-20 12:49:00 -0600

    Split ModelFactory into SpotFactory and UserFactory

--- database/factories/SpotFactory.php
+++ database/factories/SpotFactory.php
@@ -1,27 +1,4 @@
 <?php
-
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| Here you may define all of your model factories. Model factories give
-| you a convenient way to create models for testing and seeding your
-| database. Just tell the factory how a default model should look.
-|
-*/
-
-$factory->define(App\User::class, function (Faker\Generator $faker) {
-    static $password = 'testing';
-
-    return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
-        'remember_token' => str_random(10),
-    ];
-});
-
 $factory->define(App\Spot::class, function (Faker\Generator $faker) {
     $map = "[";
     $random = mt_rand(2,9);
