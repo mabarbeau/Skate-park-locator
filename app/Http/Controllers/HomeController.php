742c253add451fe0332537b09e38ffcb50fa46d6
Marc Barbeau <mabarbeau1@gmail.com>
2017-05-22 18:46:36 -0600

    +use App\Backup

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Backup;
+
 class HomeController extends Controller
 {
     /**
@@ -25,4 +27,5 @@ class HomeController extends Controller
     {
         return view('home');
     }
+
 }
