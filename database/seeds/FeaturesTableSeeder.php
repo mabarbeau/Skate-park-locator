fadab3f675d036185a70ed0cf057b14f5fb1f720
Marc Barbeau <mabarbeau1@gmail.com>
2017-05-31 17:47:19 -0600

    More features

--- database/seeds/FeaturesTableSeeder.php
+++ database/seeds/FeaturesTableSeeder.php
@@ -11,15 +11,15 @@ class FeaturesTableSeeder extends Seeder
      */
     public function run()
     {
-        $users = App\User::select('id')->inRandomOrder()->get();
-        for ($i=0; $i < 100 ; $i++) {
+        for ($i=0; $i < 300 ; $i++) {
+            $users = App\User::select('id')->inRandomOrder()->take(2)->get();
             $spot = App\Spot::inRandomOrder()->firstOrFail();
 
             $count = App\Feature::where('spot_id', $spot->id)->count();
 
             factory(App\Feature::class)->create([
-                'creator_id'=> $users[$i]->id,
-                'updater_id'=> $users[$i]->id,
+                'creator_id'=> $users[0]->id,
+                'updater_id'=> $users[1]->id,
                 'spot_id' => $spot->id,
                 'index' => $count,
              ]);
