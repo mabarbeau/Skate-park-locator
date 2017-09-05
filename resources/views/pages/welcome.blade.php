6db1fe069bc78773cddb36a125656900f97f9b0f
Marc Barbeau <mabarbeau1@gmail.com>
2017-09-04 17:09:10 -0700

    Revert "Setup vue router"
    This reverts commit 2bfd10cc89cab62125b1e22410ffea11fbc318ce.

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -3,17 +3,12 @@
   Welcome!
 @stop
 @section('content')
-  <div id="app">
-    <h1>Hello App!</h1>
-    <p>
-      <!-- use router-link component for navigation. -->
-      <!-- specify the link by passing the `to` prop. -->
-      <!-- <router-link> will be rendered as an `<a>` tag by default -->
-      <router-link to="/foo">Go to Foo</router-link>
-      <router-link to="/bar">Go to Bar</router-link>
-    </p>
-    <!-- route outlet -->
-    <!-- component matched by the route will render here -->
-    <router-view></router-view>
-  </div>
+  <div class="container">
+    <div class="jumbotron">
+      <h1>
+        Welcome!
+      </h1>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </div>
+  <div>
 @stop
