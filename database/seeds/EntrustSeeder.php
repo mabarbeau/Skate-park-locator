dfb063373cedd22475c8d80c62af99bdab79919b
Marc Barbeau <mabarbeau1@gmail.com>
2017-10-28 13:36:35 -0700

    Attach roles and permissions

--- database/seeds/EntrustSeeder.php
+++ database/seeds/EntrustSeeder.php
@@ -45,6 +45,12 @@ class EntrustSeeder extends Seeder
         ],
     ];
 
+    protected $role_permissions = [
+        'user-create',
+        'user-update',
+        'user-delete',
+    ];
+
     /**
      * Run the database seeds.
      *
@@ -54,6 +60,7 @@ class EntrustSeeder extends Seeder
     {
         $this->create('App\Role', $this->roles);
         $this->create('App\Permission', $this->permissions);
+        $this->assign($this->role_permissions);
     }
 
     protected function create($type, $data){
@@ -61,7 +68,31 @@ class EntrustSeeder extends Seeder
             if( $type::where('name', $role['name'])->count() <= 0 ){
                 $owner = new $type($role);
                 $owner->save();
+                echo "Created $type: " . $role['name'] . "\n";
+            }else{
+                echo "Found $type: " . $role['name'] . "\n";
             }
         }
     }
+
+    protected function assign($data){
+        foreach ($data as $item ) {
+            $arr = explode('-', $item);
+            $role_name = $arr[0];
+            $permission_name = $arr[1];
+
+            $role = App\Role::where('name', $role_name)->firstOrFail();
+            $count = $role->perms()->where('name', $permission_name)->count();
+            if( $count == 0 ){
+                $role = App\Role::where('name', $role_name)->firstOrFail();
+                $permission = App\Permission::where('name', $permission_name)->firstOrFail();
+                $role->perms()->attach([$permission->id]) ;
+                echo "Created pivot: $item \n";
+            }else{
+                echo "Found pivot: $item \n";
+            }
+        }
+    }
+
+
 }
