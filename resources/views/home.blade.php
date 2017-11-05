5f1df504bd124745cb7e637b1b54248213c299e1
Marc Barbeau <mabarbeau1@gmail.com>
2017-11-04 21:02:37 -0700

    Indentation

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,61 +2,62 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h1>
-                        {{ Auth::user()->name }}
-                     </h1>
-                </div>
-                <div class="panel-body">
-                    <dl class="dl-horizontal">
-                      <dt>
-                          Email
-                      </dt>
-                      <dd>
-                          {{ Auth::user()->email }}
-                      </dd>
-                      <dt>Created</dt>
-                      <dd>
-                          {{ Auth::user()->created_at }}
-                      </dd>
-                      <dt>Updated</dt>
-                      <dd>
-                          {{ Auth::user()->updated_at }}
-                      </dd>
-                      <dt>
-                          Role
-                      </dt>
-                      <dd>
-                          @role('super-admin')
-                              super-admin
-                          @endrole
-                          @role('admin')
-                              admin
-                          @endrole
-                          @role('editor')
-                              editor
-                          @endrole
-                          @role('basic')
-                              basic
-                          @endrole
-
-                      </dd>
-                    </dl>
-                    <a class="btn btn-default" href="{{ url('/logout') }}"
-                        onclick="event.preventDefault();
-                                 document.getElementById('logout-form').submit();">
-                        Logout
-                    </a>
-
-                    <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-                        {{ csrf_field() }}
-                    </form>
-                </div>
-            </div>
+  <div class="row">
+    <div class="col-md-8 col-md-offset-2">
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h1>
+            {{ Auth::user()->name }}
+           </h1>
         </div>
+        <div class="panel-body">
+          <dl class="dl-horizontal">
+            <dt>
+              Email
+            </dt>
+            <dd>
+              {{ Auth::user()->email }}
+            </dd>
+            <dt>Created</dt>
+            <dd>
+              {{ Auth::user()->created_at }}
+            </dd>
+            <dt>Updated</dt>
+            <dd>
+              {{ Auth::user()->updated_at }}
+            </dd>
+            <dt>
+              Role
+            </dt>
+            <dd>
+              @role('super-admin')
+                super-admin
+              @endrole
+              @role('admin')
+                admin
+              @endrole
+              @role('editor')
+                editor
+              @endrole
+              @role('basic')
+                basic
+              @endrole
+            </dd>
+          </dl>
+          <a
+            class="btn btn-default"
+            href="{{ url('/logout') }}"
+            onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
+          >
+            Logout
+          </a>
+
+          <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+              {{ csrf_field() }}
+          </form>
+        </div>
+      </div>
     </div>
+  </div>
 </div>
 @endsection
